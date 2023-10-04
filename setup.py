05537ce3aeb561beea375c0c813d5efe6198d330
Paras Jain <Paras.Jain@eclerx.com>
2023-10-04 17:51:43 +0530

    Requirements & Setup added

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(name="Helmet_Detection",
+      version="0.0.1",
+      author="Paras Jain",
+      author_email="Paras.Jain@eclerx.com",
+      packages=find_packages(),
+      install_requires=[])
